179e5f9cfc733a20795fd04839d7d5fbf37787d7
Jesuchristopher-Joseph <jesuinmit@gmail.com>
2022-06-30 13:50:12 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,12 @@ var box
 function setup() {
   createCanvas(400,400);
 
-  box = createSprite(200,200,40,40);
+  box = createSprite(200,200,30,30);
 }
 
 function draw() 
 {
-  background(40);
+  background(30);
   //box.fill(blue);
 
   if (keyDown(LEFT_ARROW)){
